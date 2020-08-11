9daecbd1ba01ef2e3ba1d434ddfddefd981bbcb6
Sofia <sofiamoiseyeva@gmail.com>
2020-08-11 18:31:01 +0300

    created modal window

--- app.js
+++ app.js
@@ -9,11 +9,50 @@ for(let i=0; i < addToCartButtons.length;i++) {
     })
 }
 
+//toggle like button state
+let likeButtons = document.querySelectorAll(".like");
+
+for(i = 0; i < likeButtons.length; i++){
+    likeButtons[i].addEventListener("click", function(){
+        if (likeButtons[i].classList.contains("liked")){
+            likeButtons[i].classList.remove("liked")
+        } else {
+            likeButtons[i].classList.add("liked")
+        }
+    })
+}
+
 //slider
 $('.slideshow').slick({
-    prevArrow: $('.prev'),
-    nextArrow: $('.next')
+    dots: true,
 })
 
+// modal
 
+let moreDetailsButtons = document.querySelectorAll(".moredetails");
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".close-btn");
+
+moreDetailsButtons.forEach(function(btn){
+    btn.addEventListener("click", openModal)
+})
  
+function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide")
+}
+
+closeBtn.addEventListener("click", closeModal)
+
+
+function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show")
+}
+
+modal.addEventListener("click", function(e) {
+    if(e.target === modal) {
+        closeModal()
+    }
+})
+  
